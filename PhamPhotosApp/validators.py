6159224b6adb4556188ab43f3b5037a7dc11fe71
maxwoollons <50788222+maxwoollons@users.noreply.github.com>
2021-08-18 15:49:22 +1000

    few bug fixes random images on refreash

--- PhamPhotosApp/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-
-def file_size(value):
-    filesize=value.size
-    if filesize>838860800:
-        raise ValidationError("Maximum file size 100mb")
