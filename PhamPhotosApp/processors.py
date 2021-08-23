308483e9c4f13e343d02c7abf84a10103acb5883
maxwoollons <maxwoollons@outlook.com>
2021-08-23 19:44:54 +1000

    Watermark StyleFix

--- PhamPhotosApp/processors.py
+++ PhamPhotosApp/processors.py
@@ -2,10 +2,25 @@ from PIL import ImageDraw, Image, ImageFont
 
 class Watermark(object):
     def process(self, img):
+        img = img.convert('RGBA')
         fnt = ImageFont.truetype("static/font/font.ttf", 300)
-        d = ImageDraw.Draw(img)
+        
+        txt = Image.new('RGBA', img.size, (255,255,255,0))
+        d = ImageDraw.Draw(txt)   
 
-        d.text((200,500), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,128))
-        d.text((200,200), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,255))
-        d.text((200,800), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,128))
+
+
+        
+        
+        
+        d.text((200,200), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,80))
+        
+        d.text((200,800), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,80))
+        
+        d.text((200,1400), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,80))
+        
+        d.text((200,2000), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,80))
+        d.text((200,2600), "ultimateimagination.com.au", font=fnt, fill=(255,255,255,80))
+        img = Image.alpha_composite(img, txt) 
+        
         return img
